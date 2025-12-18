f3158499ee738f65ca9979612d8b4461fea987b8
nival <nivahmichael@gmail.com>
2025-12-18 19:12:24 +0300

    implemnted check-ups

--- app/src/main/java/com/example/Varsani/IntendedParent/TrackCheckups.java
+++ app/src/main/java/com/example/Varsani/IntendedParent/TrackCheckups.java
@@ -41,7 +41,6 @@ public class TrackCheckups extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
